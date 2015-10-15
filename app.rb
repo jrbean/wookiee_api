c7b88dcd9e727fb25d73c049b641441ccf2ff4a8
Justin Bean <beanjustinr@gmail.com>
2015-10-15 16:19:05 -0400

    implemented search, removed case sensitivity

--- app.rb
+++ app.rb
@@ -4,9 +4,30 @@ require './lib/all'
 
 class App < Sinatra::Base
 
+  def check_name name
+      name_to_check = name
+
+      results_arr = []
+      Character.all.each do |c|
+        if c[:name].downcase.include? name_to_check.downcase
+          {
+            name: c.name,
+            species: c.species,
+            height: c.height,
+            weight: c.weight
+            }
+          #c.merge(affiliations: c.affiliations.map { |a| a[:name] })
+          results_arr << c
+        end
+      end
+      { results: results_arr }.to_json
+    end
+
+
+
   get "/:name" do
-    char_name = params[:name]
-    n = Character.find_by(name: char_name)
+    check_name params[:name]
+    # n = Character.find_by(name: char_name)
 
     # user_hashes = []
     # t.users.each do |u|
@@ -14,13 +35,13 @@ class App < Sinatra::Base
     # end
     #result_hash = n.characters.map do |i|
       #{ name: i.name, species: i.species, height: i.height, weight: i.weight }
-  { results:  {
-    name: n.name,
-    species: n.species,
-    height: n.height,
-    weight: n.weight
-    }
-  }.to_json
+  # { results:  {
+  #   name: n.name,
+  #   species: n.species,
+  #   height: n.height,
+  #   weight: n.weight
+  #   }
+  # }.to_json
   end
 
 
